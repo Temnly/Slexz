a476af7a59c0ebaf4807748dd0a01e6cf033fca0
Temnly <76433621+Temnly@users.noreply.github.com>
2021-01-10 18:12:57 +0100

    Updated Ping File | NOW MULTIPLE LANGUAGE

--- commands/ping.py
+++ commands/ping.py
@@ -1,5 +1,17 @@
 from os import system
 
 def general():
- url = input('Enter URL: ')
- system("ping " + url)
+
+
+ with open("etc/settings/language.txt", "r") as f:
+      text = f.read().strip()
+
+
+ if text == "en":
+      url = input('Enter URL: ')
+      system("ping " + url)
+ elif text == "de":
+      url = input('Gebe deine gewünschte URL ein: ')
+      system("ping " + url)
+ else:
+    print("Error. Unsupported Language!")
