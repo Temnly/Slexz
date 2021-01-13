f26905a5a38525ad08ab47d6399baac88118b6d9
LAKSHAY184 <57940742+LAKSHAY184@users.noreply.github.com>
2021-01-13 12:01:57 +0530

    Solved an issue in downloader.py

--- commands/downloader.py
+++ commands/downloader.py
@@ -4,10 +4,10 @@ import requests
 def general():
 
  value = input("What do you want to download?")
- download_ask = input("\nPLEASE NOTE: You have to type in the file extension as well.\n\nAs what do you want to save it? ")
+ download_ask = input("\nPLEASE NOTE: You have to type in the file extension and the full path of the download as well.\n\nAs what do you want to save it? ")
  r = requests.get(value)
 
- with open('/Users/tim/Downloads/' + download_ask, 'wb') as f:
+ with open(download_ask, 'wb') as f:
     f.write(r.content)
 
 
