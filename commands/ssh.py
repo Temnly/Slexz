f334a580d50b3400d5f9d8b787224b05bf95691a
ZenTixDeV <77199666+ZenTixDeV@users.noreply.github.com>
2021-01-13 15:52:37 +0100

    SSH Script

---
+++ commands/ssh.py
@@ -0,0 +1,30 @@
+import paramiko 
+
+#input
+host = input("Hostname -> ")
+
+port = input("Port -> ")
+
+username = input("Username -> ")
+
+password = input("Password -> ")
+
+while True:
+ command = input("SSH -> ")
+
+
+ ssh = paramiko.SSHClient()
+
+ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ ssh.connect(host, port, username, password)
+
+
+ stdin, stdout, stderr = ssh.exec_command(command)
+
+ lines = stdout.readlines()
+
+ print(lines)
+
+
+
