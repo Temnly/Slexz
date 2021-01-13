df9712173d37392ad6e4b9e2343e53d3d24d8732
Temnly <76433621+Temnly@users.noreply.github.com>
2021-01-13 15:39:11 +0100

    Update list.py

--- commands/list.py
+++ commands/list.py
@@ -3,13 +3,26 @@ import os
 
 def general():
  
- print(" ")
- print(" ")
- print("Type in the path you want to list")
- print(" ")
- print(" ")
- directory = input("Which directory do you want to list? ->  ")
- 
+ with open("etc/settings/language.txt", "r") as f:
+      text = f.read().strip()
+ if text == "en":
+      print(" ")
+      print(" ")
+      print("Type in the path you want to list")
+      print(" ")
+      print(" ")
+      directory = input("Which directory do you want to list? ->  ")
+ elif text == "de":
+      print(" ")
+      print(" ")
+      print("Gebe den Pfad ein, den du auflisten möchtest")
+      print(" ")
+      print(" ")
+      directory = input("Welchen Ordner möchtest du auflisten lassen? ->  ")
+ else:
+    print("Error. Unsupported Language!")
 
  stuff = os.listdir(directory)
  print(stuff)
+
+
