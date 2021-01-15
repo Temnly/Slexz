6b621d033d543d76caa671042ab6cf66b2b0ce9c
kanishk115 <71209473+kanishk115@users.noreply.github.com>
2021-01-15 22:42:12 +0530

    Delete info.py

--- commands/info.py
+++
@@ -1,8 +0,0 @@
-import socket
-def general():
-    hostname = socket.gethostname() 
-    IPAddr = socket.gethostbyname(hostname) 
-    print(" ")
-    print("Your Computer Name is: " + hostname) 
-    print("Your Computer IP Address is: " + IPAddr) 
-    print(" ")
